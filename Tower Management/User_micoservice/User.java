8a2f1f95c93d91384ab4bf04660942d4bf3adc33
Sri Ranjani R <123641341+SriRanjani-27@users.noreply.github.com>
2024-08-29 23:49:30 +0530

    Add files via upload

---
+++ Tower Management/User_micoservice/User.java
@@ -0,0 +1,34 @@
+package com.verizon.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_id_seq", allocationSize = 1, initialValue = 500001)
+    private Long userId;
+    private String username;
+    private String password;
+    private String email;
+    private String specialization;
+    private String location;
+    private String pincode;
+    private String role;
+    private Boolean activeStatus;
+    private Boolean deletedStatus;
+    private String name;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
