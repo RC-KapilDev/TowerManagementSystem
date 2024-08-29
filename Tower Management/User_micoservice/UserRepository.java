b6ab5dc3177a57e94efb8cc925cd59e24fcaac10
Sri Ranjani R <123641341+SriRanjani-27@users.noreply.github.com>
2024-08-30 00:27:47 +0530

    Delete Tower Management/User_micoservice/UserRepository.java

--- Tower Management/User_micoservice/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.verizon.userservice.repo;
-
-import com.verizon.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
